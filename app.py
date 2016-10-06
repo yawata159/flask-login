6264391654b071d1a978c4cef9e10621be684390
AWinnerIsYou <seijiyawata@gmail.com>
2016-10-06 19:32:12 -0400

    Reorganize

--- app.py
+++ app.py
@@ -4,6 +4,29 @@ import csv
 
 app = Flask(__name__)
 
+def register(user, password):
+    user_list = [row[0] for row in csv.reader(open("data/passwd.csv","r"))]
+    if user in user_list:
+        return render_template("home.html", user_exists = True, user = user)
+    hashed_pw = sha512(password.encode("utf-8")).hexdigest()
+    new_row = '"' + user + '","' + hashed_pw + '"\n'
+    append_file = open("data/passwd.csv", "a")
+    append_file.write(new_row)
+    append_file.close()
+    return render_template("home.html", user_created = True)
+
+
+def login(user, password):
+    user_pw_dict ={row[0]:row[1] for row in csv.reader(open("data/passwd.csv", "r"))}
+    if user in user_pw_dict:
+        hashed_pw = sha512(password.encode("utf-8")).hexdigest()                
+        if hashed_pw == user_pw_dict[user]:
+            return render_template("loginattempt.html", success = True)
+        else:
+            return render_template("loginattempt.html", wrong_passwd = True)
+    else:
+        return render_template("loginattempt.html", wrong_user = True)            
+    
 
 @app.route("/")
 @app.route("/login/")
@@ -20,31 +43,10 @@ def auth():
     
     if request.method == "POST":
         if 'register' in request.form:
-            user_list = [row[0] for row in csv.reader(open("data/passwd.csv","r"))]
-            
-            if user in user_list:
-                return render_template("home.html", user_exists = True, user = user)
-
-            hashed_pw = sha512(password.encode("utf-8")).hexdigest()
-            new_row = '"' + user + '","' + hashed_pw + '"\n'
-
-            append_file = open("data/passwd.csv", "a")
-            append_file.write(new_row)
-            append_file.close()
-
-            return render_template("home.html", user_created = True)
-            
+            return register(user,password)
+                        
         if "login" in request.form:
-
-            user_pw_dict ={row[0]:row[1] for row in csv.reader(open("data/passwd.csv", "r"))}
-            if user in user_pw_dict:
-                hashed_pw = sha512(password.encode("utf-8")).hexdigest()                
-                if hashed_pw == user_pw_dict[user]:
-                    return render_template("loginattempt.html", success = True)
-                else:
-                    return render_template("loginattempt.html", wrong_passwd = True)
-            else:
-                return render_template("loginattempt.html", wrong_user = True)            
+            return login(user,password)
     else:
         return render_template("home.html")
         
