0bce0affca23defec8a9ec4907e3e7187796bde0
yawata159 <seijiyawata@gmail.com>
2016-10-06 20:33:05 -0400

    Add sessions

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, session, redirect
 from hashlib import sha512
 import csv
 
 app = Flask(__name__)
+app.secret_key = '\x96\xfb"\xed\xcc\xa3J\xb1O\x8dGf\x916!\xa0\x9f#T2S\xb3a\xf0\xa7\xed\xb8\xb4\xa7\x08w\xe2'
 
 def register(user, password):
     user_list = [row[0] for row in csv.reader(open("data/passwd.csv","r"))]
     if user in user_list:
-        return render_template("home.html", user_exists = True, user = user)
+        return render_template("home.html", message = "The username " + user + " is already taken")
     hashed_pw = sha512(password.encode("utf-8")).hexdigest()
     new_row = '"' + user + '","' + hashed_pw + '"\n'
     append_file = open("data/passwd.csv", "a")
     append_file.write(new_row)
     append_file.close()
-    return render_template("home.html", user_created = True)
+    return render_template("home.html", message = "Your account has been created" )
 
 
 def login(user, password):
@@ -21,17 +22,41 @@ def login(user, password):
     if user in user_pw_dict:
         hashed_pw = sha512(password.encode("utf-8")).hexdigest()                
         if hashed_pw == user_pw_dict[user]:
-            return render_template("loginattempt.html", success = True)
+            session['user'] = user
+            return redirect(url_for('welcome'))
         else:
-            return render_template("loginattempt.html", wrong_passwd = True)
+            return render_template("home.html", message = "Wrong Password")            
     else:
-        return render_template("loginattempt.html", wrong_user = True)            
+        return render_template("home.html", message = "That username is not registered (yet!)")
     
 
 @app.route("/")
+def root():
+    if 'user' in session:
+        return redirect(url_for('welcome'))
+    else:
+        return redirect(url_for('logregister'))
+
+
+@app.route('/home/')
+def welcome():
+    if 'user' in session:
+        return render_template('welcome.html')
+    else:
+        return redirect(url_for('logregister'))
+
 @app.route("/login/")
-def start():
-    return render_template("home.html")
+def logregister():
+    if 'user' in session:
+        return redirect(url_for('welcome'))
+    else:
+        return render_template('home.html')
+
+
+@app.route("/logout/", methods=["POST"])
+def logout():
+    session.pop('user')
+    return redirect(url_for('root'))
 
 @app.route("/authenticate/", methods=["POST"])
 def auth():
@@ -39,7 +64,7 @@ def auth():
     password = request.form["password"]
 
     if user == "" or password == "":
-        return render_template("home.html", empty_field = True)
+        return render_template("home.html", message = "Username and/or password fields can't be empty")
     
     if request.method == "POST":
         if 'register' in request.form:
